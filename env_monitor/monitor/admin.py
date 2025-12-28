3cc77470935d610f78c514bb88e74436c71a2ba8
Mansi Savdekar <60616521+MansiSavdekar@users.noreply.github.com>
2025-12-28 19:53:48 +0530

    Delete env_monitor directory

--- env_monitor/monitor/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import AirQuality, WaterQuality
-
-admin.site.register(AirQuality)
-admin.site.register(WaterQuality)
--- env_monitor/monitor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import dashboard, fetch_air, air_api, water_api
-
-urlpatterns = [
-    path('', dashboard),
-    path('fetch-air/', fetch_air),
-    path('api/air/', air_api),
-    path('api/water/', water_api),
-]
